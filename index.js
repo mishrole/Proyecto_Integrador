efe1449f7cd3ce48f48ede81933d9df37b95c7de
Mitchell Rodríguez <mitrodle@gmail.com>
2021-04-02 18:16:12 -0500

    Home

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
     const cards = document.querySelectorAll('.card');
     
     for(const card of cards) {
-        card.addEventListener("click", () => {
-            window.location.href = "./Shop_Item.html"
+        card.addEventListener('click', () => {
+            window.location.href = './Shop_Item.html';
         });
     }
+
+    const btnSingIn = document.querySelector('.btn__Sing-In');
+    btnSingIn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = '../_Home/Home.html';
+    })
+
 })();
