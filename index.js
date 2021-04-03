08c272c15cdda7f6a14d2f25965f3ded451aeb17
Mitchell Rodríguez <mitrodle@gmail.com>
2021-04-03 17:32:06 -0500

    Chartjs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
   const btnSingIn = document.querySelector('#btn-signin');
   const inputBornDate = document.querySelector('#pet__bornDate');
   const calendar = document.querySelector('#calendar');
+  const ctx = document.querySelector('#myChart');
 
   if(productCards) {
       // Click event on all productCards
@@ -71,6 +72,68 @@
       fullCalendar.render();
   }
 
+  const labels = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+  ];
+  
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+          label: 'Pedidos',
+          data: [8, 10, 14, 20],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        },
+        {
+          label: 'Citas',
+          data: [15, 12, 6, 4],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+  });
     
 
 
