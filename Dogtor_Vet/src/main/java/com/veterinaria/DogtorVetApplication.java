330fa322c2bb4538bf4099991fa1f8d25bdc32ed
Mitchell Rodríguez <mitrodle@gmail.com>
2021-04-06 22:48:33 -0500

    [Spring]: Initial Project

---
+++ Dogtor_Vet/src/main/java/com/veterinaria/DogtorVetApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogtorVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogtorVetApplication.class, args);
+	}
+
+}
