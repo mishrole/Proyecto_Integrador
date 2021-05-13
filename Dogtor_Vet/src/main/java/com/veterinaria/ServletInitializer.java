0824de99cb6e168eff48ce2648ea2d7951ec81f2
Mitchell Rodríguez <mitrodle@gmail.com>
2021-05-13 01:26:17 -0500

    [Heroku]: Test 01:26

--- Dogtor_Vet/src/main/java/com/veterinaria/ServletInitializer.java
+++ Dogtor_Vet/src/main/java/com/veterinaria/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.veterinaria;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -8,6 +9,10 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DogtorVetApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServletInitializer.class, args);
 	}
 
 }
